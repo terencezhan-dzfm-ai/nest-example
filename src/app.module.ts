8b0d2c7e2e080a9dc1051cc593cc675594041005
Terence Zhan <terencezhan2023@gmail.com>
2025-07-16 17:43:07 +0800

    add groups and roles modules with basic CRUD functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, RolesModule, GroupsModule],
   controllers: [AppController],
   providers: [AppService],
 })
